5394e6f1b4051b8c56fc7af76e1ee6d4710a3091
Ritik Kumar <ritikraj2018634@gmail.com>
2023-09-24 04:37:55 +0530

    Added city-service

---
+++ src/repository/index.js
@@ -0,0 +1,7 @@
+// it will help us for making our code clean 
+// we will exprt all the repo.. from here in a key value pair form 
+
+module.exports = {
+    cityRepository : require('./city-repository')
+
+}
